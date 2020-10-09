8ee238cac2fe66209911ace7c6cb48dfe174db47
juancaruizc <juancruizcruz@gmail.com>
2020-10-08 21:22:53 -0700

    day 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from 'axios'
 import Picture from './Components/Picture'
+import { ThemeProvider } from 'styled-components'
+
 
 function App() {
   const [nasaData, setNasaData] = useState([])
--- src/Components/Picture.js
+++ src/Components/Picture.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledPicture = styled.div`
+    width:50%;
+    height:50%;
+`
+
 
 const Picture = (props) => {
-    
+    const {theme} = props
     return (
-        <div>
+        <StyledPicture>
             <img src = {props.nasaData.url} />
-        </div>
+        </StyledPicture>
     )
 }
 
