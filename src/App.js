f310407e51be7d9eb4e81d3a54ad48b4ab5fb942
juancaruizc <juancruizcruz@gmail.com>
2020-11-04 13:52:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <p>
         Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        app! Have fun{' '}
+        <span role='img' aria-label='go!'>
+          🚀
+        </span>
+        !
       </p>
     </div>
   );
 }
 
 export default App;
+
+//test
